d05115365ac55682580a31183596535cd67c324d
Xinyu Qiu <qiux1@seattleu.edu>
2022-01-18 19:39:51 -0800

    Added Comments to ItemModel.cs

--- Mine/Mine/Models/ItemModel.cs
+++ Mine/Mine/Models/ItemModel.cs
@@ -4,10 +4,16 @@ namespace Mine.Models
 {
     public class ItemModel
     {
+        //The Id for the Item
         public string Id { get; set; }
+
+        //The Display for the Item
         public string Text { get; set; }
+
+        //The Description for the Item
         public string Description { get; set; }
 
+        //The Value of the Item +9 Damge
         public int Value { get; set; } = 0;
     }
 }
