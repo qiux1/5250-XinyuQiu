9b5331ae06b4d88c77ab7d403faf6cc1b927a3e9
Xinyu Qiu <qiux1@seattleu.edu>
2022-01-18 19:30:14 -0800

    Added Value attribute to ItemModel.cs

--- Mine/Mine/Models/ItemModel.cs
+++ Mine/Mine/Models/ItemModel.cs
@@ -7,5 +7,7 @@ namespace Mine.Models
         public string Id { get; set; }
         public string Text { get; set; }
         public string Description { get; set; }
+
+        public int Value { get; set; } = 0;
     }
 }
