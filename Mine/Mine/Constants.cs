637cf23125d1e480a5cd73448e46d63f95d3c729
Xinyu Qiu <qiux1@seattleu.edu>
2022-01-30 11:37:31 -0800

    Add necessary const in Constants.cs. Changed in Constants.cs

--- Mine/Mine/Constants.cs
+++ Mine/Mine/Constants.cs
@@ -1,4 +1,12 @@
 public static class Constants
 {
+    public const string DatabaseFilename = "TodoSQLite.db3";
 
+    public const SQLite.SQLiteOpenFlags Flags =
+        //Open the database with read/write mode
+        SQLite.SQLiteOpenFlags.ReadWrite |
+        //Create the database if it does not exist
+        SQLite.SQLiteOpenFlags.Create |
+        //Enable multi-thread database access
+        SQLite.SQLiteOpenFlags.SharedCache;
 }
