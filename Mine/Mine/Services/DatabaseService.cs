a44b72bc43f7bff039b4b21e267d2d956ff9ca71
Xinyu Qiu <qiux1@seattleu.edu>
2022-01-31 20:40:49 -0800

    Added DatabaseService to services in shared project, DatabaseService.cs

--- Mine/Mine/Services/DatabaseService.cs
+++ Mine/Mine/Services/DatabaseService.cs
@@ -14,5 +14,33 @@ namespace Mine.Services
         {
             return new SQLiteAsyncConnection(Constants.DatabasePath, Constants.Flags);
         });
+
+        //Create a variable to store the value from the initialization
+        static SQLiteAsyncConnection Database => lazyInitializer.Value;
+        //Boolean status variable to check if the connection is initialized
+        static bool initialized = false;
+
+        //Constructor
+        public DatabaseService()
+        {
+            InitializeAsnyc().SafeFireAndForget(false);
+        } 
+
+        //Task to initialized the DatabaseService
+        async Task InitializeAsnyc()
+        {
+            //If the DatabaseService is nto initialized
+            if (!initialized)
+            {
+                if (!Database.TableMappings.Any(mbox =>m.MappedType.Name == typeof(ItemModel).Name))
+                {
+                    await Database.CreateTablesAsync(CreateFlags.None, typeof(ItemModel)).ConfigureAwait(false);
+                }
+                //Change the status variable to true
+                initialized = true;  
+            }
+        }
+
+
     }
 }
