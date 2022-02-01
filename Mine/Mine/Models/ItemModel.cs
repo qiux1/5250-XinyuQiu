db7c13717aaa07627328b9187453120b5aba5fd5
Xinyu Qiu <qiux1@seattleu.edu>
2022-02-01 12:54:52 -0800

    Added PrimaryKey to ID as guid in ItemModel.cs. Changed in ItemModels.cs

--- Mine/Mine/Models/ItemModel.cs
+++ Mine/Mine/Models/ItemModel.cs
@@ -1,11 +1,16 @@
 ﻿using System;
+using SQLite;
 
 namespace Mine.Models
 {
+    /// <summary>
+    /// Items for the Characters and Monsters to use
+    /// </summary>
     public class ItemModel
     {
         //The Id for the Item
-        public string Id { get; set; }
+        [PrimaryKey]
+        public string Id { get; set; } = Guid.NewGuid().ToString();
 
         //The Display for the Item
         public string Text { get; set; }
