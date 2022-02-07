1ed4267966859ff96b90efa06426b0e1a98ea13a
Xinyu Qiu <qiux1@seattleu.edu>
2022-02-07 13:23:00 -0800

    Added RollDice_Valid_Roll_1_Dice_6 to DiceHelperUnitTest.cs

--- UnitTests/Helpers/DiceHelperUnitTest.cs
+++ UnitTests/Helpers/DiceHelperUnitTest.cs
@@ -7,6 +7,9 @@ namespace UnitTests.Helpers
     [TestFixture]
     public class DiceHelperTests
     {
+        /// <summary>
+        /// Unit Test for Invalid roll
+        /// </summary>
         [Test]
         public void RollDice_Invalid_Roll_Zero_Should_Return_Zero()
         {
@@ -19,6 +22,24 @@ namespace UnitTests.Helpers
 
             // Assert 
             Assert.AreEqual(0, result);
+        }
+
+        /// <summary>
+        /// Unit Test for Invalid roll
+        /// </summary>
+        [Test]
+        public void RollDice_Valid_Roll_1_Dice_6_Should_Return_Between_1_And_6()
+        {
+            // Arrange
+
+            // Act
+            var result = DiceHelper.RollDice(1, 6);
+
+            // Reset
+
+            // Assert 
+            Assert.AreEqual(true, result >= 1);
+            Assert.AreEqual(true, result <= 6);
         }
     }
 }
