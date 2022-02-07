1b00b3901256f0dc1bf2e38e8bce9d2025b82811
Xinyu Qiu <qiux1@seattleu.edu>
2022-02-07 13:20:11 -0800

    Added RollDice Invalid Roll Zero to DiceHelperUnitTests.cs

--- UnitTests/Helpers/DiceHelperUnitTest.cs
+++ UnitTests/Helpers/DiceHelperUnitTest.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Mine.Models;
+using NUnit.Framework;
+using Mine.Helpers;
 
 namespace UnitTests.Helpers
 {
-    internal class DiceHelperUnitTest
+    [TestFixture]
+    public class DiceHelperTests
     {
+        [Test]
+        public void RollDice_Invalid_Roll_Zero_Should_Return_Zero()
+        {
+            // Arrange
+
+            // Act
+            var result = DiceHelper.RollDice(0, 1);
+
+            // Reset
+
+            // Assert 
+            Assert.AreEqual(0, result);
+        }
     }
 }
+
